a2ecc039f76b638d72500b51a5b9a8a0efa3a8e0
venkat <devopsaws84s@gmail.com>
2025-12-10 10:17:58 +0530

    frontend

--- Frontend.sh
+++ Frontend.sh
@@ -41,7 +41,7 @@ VALIDATE(){
     fi
 }
 
-dnf module disable nginx-y &>>$LOG_FILE
+dnf module disable nginx -y &>>$LOG_FILE
 VALIDATE $? "DISABLE OLD MODULE OF NGINX" 
 NEWLINE
 dnf module enable nginx:1.24 -y &>>$LOG_FILE
