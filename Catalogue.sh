b756ff3af7983afc7eec29378764c0afb8fe99cb
venkat <devopsaws84s@gmail.com>
2025-12-11 09:29:53 +0530

    catalogue

--- Catalogue.sh
+++ Catalogue.sh
@@ -44,129 +44,129 @@ VALIDATE(){
 }
 
 
-dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "Disabling default nodejs"
-
-dnf module enable nodejs:20 -y &>>$LOG_FILE
-VALIDATE $? "Enabling nodejs:20"
-
-dnf install nodejs -y &>>$LOG_FILE
-VALIDATE $? "Installing nodejs:20"
-
-id roboshop
-if [ $? -ne 0 ]
-then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-    VALIDATE $? "Creating roboshop system user"
-else
-    echo -e "System user roboshop already created ... $Y SKIPPING $N"
-fi
-
-mkdir -p /app 
-VALIDATE $? "Creating app directory"
-
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading Catalogue"
-
-rm -rf /app/*
-cd /app 
-unzip /tmp/catalogue.zip &>>$LOG_FILE
-VALIDATE $? "unzipping catalogue"
-
-npm install &>>$LOG_FILE
-VALIDATE $? "Installing Dependencies"
-
-cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
-VALIDATE $? "Copying catalogue service"
-
-systemctl daemon-reload &>>$LOG_FILE
-systemctl enable catalogue  &>>$LOG_FILE
-systemctl start catalogue
-VALIDATE $? "Starting Catalogue"
-
-cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
-dnf install mongodb-mongosh -y &>>$LOG_FILE
-VALIDATE $? "Installing MongoDB Client"
-
-STATUS=$(mongosh --host mongodb.daws84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
-if [ $STATUS -lt 0 ]
-then
-    mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
-    VALIDATE $? "Loading data into MongoDB"
-else
-    echo -e "Data is already loaded ... $Y SKIPPING $N"
-fi
-
-
-
-
 # dnf module disable nodejs -y &>>$LOG_FILE
-# VALIDATE $? "DISABLE OLD MODULE OF NODEJS" 
-# NEWLINE
+# VALIDATE $? "Disabling default nodejs"
+
 # dnf module enable nodejs:20 -y &>>$LOG_FILE
-# VALIDATE $? "ENABLE MODULE20 OF NODEJS"
-# NEWLINE
+# VALIDATE $? "Enabling nodejs:20"
+
 # dnf install nodejs -y &>>$LOG_FILE
-# VALIDATE $? "INSTALLATION OF NODEJS"
-# NEWLINE
+# VALIDATE $? "Installing nodejs:20"
 
 # id roboshop
 # if [ $? -ne 0 ]
-# then 
-#    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-#    VALIDATE $? "CREATING APPLICATION USER"
+# then
+#     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+#     VALIDATE $? "Creating roboshop system user"
 # else
-#    echo -e "$Y USER ALREADY EXIST, SKIPPING $N"
+#     echo -e "System user roboshop already created ... $Y SKIPPING $N"
 # fi
 
-# NEWLINE
-# mkdir -p /app &>>$LOG_FILE
-# VALIDATE $? "APP DIRECTORY CREATION"
-# NEWLINE
+# mkdir -p /app 
+# VALIDATE $? "Creating app directory"
 
-# rm -rf /app/* # removing any contents in the app directory if we rerun the script
+# curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+# VALIDATE $? "Downloading Catalogue"
 
-# curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &>>$LOG_FILE
-# VALIDATE $? "DOWNLOADING THE CATALOGUE APPLICATION CODE"
-# NEWLINE
-
-# cd /app &>>$LOG_FILE
+# rm -rf /app/*
+# cd /app 
 # unzip /tmp/catalogue.zip &>>$LOG_FILE
-# sleep 5
-# echo -e "$Y UNZIPPING THE ZIP FILE $N"
-# NEWLINE
+# VALIDATE $? "unzipping catalogue"
 
 # npm install &>>$LOG_FILE
-# VALIDATE $? "UNZIP & DEPENDECNCIES  INSTALLATION"
-# NEWLINE
+# VALIDATE $? "Installing Dependencies"
 
-# cp $SCRIPT_DIRECTORY/catalogue.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
-# VALIDATE $? "COPYING CATALOGUE SERVICE"
-# NEWLINE
+# cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+# VALIDATE $? "Copying catalogue service"
 
 # systemctl daemon-reload &>>$LOG_FILE
-# sleep 5
-# systemctl enable catalogue &>>$LOG_FILE
-# systemctl start catalogue &>>$LOG_FILE
-# VALIDATE $? "RELOAD,ENABLING & STARTING OF CATALOGUE SERVICE" 
-# NEWLINE
+# systemctl enable catalogue  &>>$LOG_FILE
+# systemctl start catalogue
+# VALIDATE $? "Starting Catalogue"
 
-# cp $SCRIPT_DIRECTORY/mongodb.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
-# VALIDATE $? "COPYING MONGODB REPO"
-# NEWLINE
-
+# cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
 # dnf install mongodb-mongosh -y &>>$LOG_FILE
-# VALIDATE $? "INSTALLING MONGODB CLIENT"
-# NEWLINE
+# VALIDATE $? "Installing MongoDB Client"
 
-# STATUS=$(mongosh --host mongodb.devopsaws.store --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+# STATUS=$(mongosh --host mongodb.daws84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 # if [ $STATUS -lt 0 ]
 # then
-#     mongosh --host mongodb.devopsaws.store </app/db/master-data.js &>>$LOG_FILE
-#     VALIDATE $? "LOADING DATA INTO MONGODB"
+#     mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
+#     VALIDATE $? "Loading data into MongoDB"
 # else
-#     echo -e "$Y DATA IS ALREADY LOADED SO SKIPPING $N"
+#     echo -e "Data is already loaded ... $Y SKIPPING $N"
 # fi
+
+
+
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "DISABLE OLD MODULE OF NODEJS" 
+NEWLINE
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "ENABLE MODULE20 OF NODEJS"
+NEWLINE
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "INSTALLATION OF NODEJS"
+NEWLINE
+
+id roboshop
+if [ $? -ne 0 ]
+then 
+   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+   VALIDATE $? "CREATING APPLICATION USER"
+else
+   echo -e "$Y USER ALREADY EXIST, SKIPPING $N"
+fi
+
+NEWLINE
+mkdir -p /app &>>$LOG_FILE
+VALIDATE $? "APP DIRECTORY CREATION"
+NEWLINE
+
+rm -rf /app/* # removing any contents in the app directory if we rerun the script
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &>>$LOG_FILE
+VALIDATE $? "DOWNLOADING THE CATALOGUE APPLICATION CODE"
+NEWLINE
+
+cd /app &>>$LOG_FILE
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+sleep 5
+echo -e "$Y UNZIPPING THE ZIP FILE $N"
+NEWLINE
+
+npm install &>>$LOG_FILE
+VALIDATE $? "UNZIP & DEPENDECNCIES  INSTALLATION"
+NEWLINE
+
+cp $SCRIPT_DIRECTORY/catalogue.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
+VALIDATE $? "COPYING CATALOGUE SERVICE"
+NEWLINE
+
+systemctl daemon-reload &>>$LOG_FILE
+sleep 5
+systemctl enable catalogue &>>$LOG_FILE
+systemctl start catalogue &>>$LOG_FILE
+VALIDATE $? "RELOAD,ENABLING & STARTING OF CATALOGUE SERVICE" 
+NEWLINE
+
+cp $SCRIPT_DIRECTORY/mongodb.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
+VALIDATE $? "COPYING MONGODB REPO"
+NEWLINE
+
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "INSTALLING MONGODB CLIENT"
+NEWLINE
+
+STATUS=$(mongosh --host mongodb.devopsaws.store --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+if [ $STATUS -lt 0 ]
+then
+    mongosh --host mongodb.devopsaws.store </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "LOADING DATA INTO MONGODB"
+else
+    echo -e "$Y DATA IS ALREADY LOADED SO SKIPPING $N"
+fi
 
 
 
