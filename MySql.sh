74edd71d719c4797b45a89ff70736df7a48e67dc
venkat <devopsaws84s@gmail.com>
2025-12-12 20:07:00 +0530

    COMMON CODE

--- MySql.sh
+++ MySql.sh
@@ -16,8 +16,8 @@ systemctl start mysqld  &>>$LOG_FILE
 VALIDATE $? "ENABLING & STARTING OF $APP_NAME"
 NEWLINE
 echo -e "$Y Please SETUP $APP_NAME PASSWORD $N"
-read -s $APP_NAME_ROOT_PASSWORD
-$app_name_secure_installation --set-root-pass $APP_NAME_ROOT_PASSWORD
+read -s MYSQL_ROOT_PASSWORD
+mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD
 VALIDATE $? "ROOT PASSWORD FOR $APP_NAME"
 NEWLINE
 
