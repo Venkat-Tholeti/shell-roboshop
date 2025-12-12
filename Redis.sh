2350a499d0079b307cfb369f67f5af8efbb7a5e7
venkat <devopsaws84s@gmail.com>
2025-12-12 10:50:36 +0530

    redis

---
+++ Redis.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript.logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo -e "$G script started executing at $(date)" &>>$LOG_FILE
+echo -e "$Y Logs stored at  $LOG_FILE"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+   echo -e "$R ERROR: USER NEED TO SWITCH TO ROOT ACCESS $N" | tee -a $LOG_FILE
+   exit 1 
+else
+   echo -e "$G USER HAS ROOT PRIVILEGES $N" | tee -a $LOG_FILE
+fi
+
+#FUNCTION NAME WE GAVE AS VALIDATE & NEWLINE (name our choice)
+# we can provide arguments to function as we do it for script
+#VALIDATE $? MYSQL  --> here 1st argument is exit status $1 = $?, 2nd argument is what we tried to install $2 =MYSL, PYTHON3 , NGINX
+
+NEWLINE(){
+      echo ""
+}
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$G $2 IS SUCCESS $N" | tee -a $LOG_FILE
+    else 
+        echo -e "$R $2 IS FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+
+dnf module disable redis -y &>>$LOG_FILE
+VALIDATE $? "DISABLING REDIS MODULE"
+NEWLINE
+dnf module enable redis:7 -y &>>$LOG_FILE
+VALIDATE $? "ENABLING REDIS 7 MODULE"
+NEWLINE
+dnf install redis -y &>>$LOG_FILE
+VALIDATE $? "INSTALLING REDIS"
+NEWLINE
+sed -i 's/127.0.0.1/0.0.0.0/g' '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>$LOG_FILE
+VALIDATE $? "EDITING REDIS CONF FILE FOR REMOTE CONNECTIONS AND PROTECT MODE CHANGES"
+NEWLINE
+systemctl enable redis &>>$LOG_FILE
+systemctl start redis  &>>$LOG_FILE
+VALIDATE $? "ENABLE & START REDIS"
+
+END_TIME=$(date +$s)
+TOTAL_TIME=$(($END_TIME - $START_TIME))
+
+echo "$Y Total time taken to execute the script is $TOTAL_TIME $N"
+
+
+
+
+
+
+
