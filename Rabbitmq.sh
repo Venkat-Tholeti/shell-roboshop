c32653ddc5df3923f7b4a9dc2b86c326023e7df5
venkat <devopsaws84s@gmail.com>
2025-12-12 20:29:08 +0530

    COMMON CODE

--- Rabbitmq.sh
+++ Rabbitmq.sh
@@ -1,58 +1,23 @@
 #!/bin/bash
 
-START_TIME=$(date +%s)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/shellscript.logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIRECTORY=$PWD
+source ./Common.sh
+app_name=rabbitmq   
+APP_NAME=RABBITMQ
+ROOTACCESS_CHECK
+NEWLINE
 
-mkdir -p $LOGS_FOLDER
-echo -e "$G script started executing at $(date)" &>>$LOG_FILE
-echo -e "$Y Logs stored at  $LOG_FILE"
 
-USERID=$(id -u)
-
-if [ $USERID -ne 0 ]
-then 
-   echo -e "$R ERROR: USER NEED TO SWITCH TO ROOT ACCESS $N" | tee -a $LOG_FILE
-   exit 1 
-else
-   echo -e "$G USER HAS ROOT PRIVILEGES $N" | tee -a $LOG_FILE
-fi
-
-#FUNCTION NAME WE GAVE AS VALIDATE & NEWLINE (name our choice)
-# we can provide arguments to function as we do it for script
-#VALIDATE $? MYSQL  --> here 1st argument is exit status $1 = $?, 2nd argument is what we tried to install $2 =MYSL, PYTHON3 , NGINX
-
-NEWLINE(){
-      echo ""
-}
-
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e "$G $2 IS SUCCESS $N" | tee -a $LOG_FILE
-    else 
-        echo -e "$R $2 IS FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
-
-cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
+cp $app_name.repo /etc/yum.repos.d/$app_name.repo &>>$LOG_FILE
 VALIDATE $? "COPYING THE REPO"
 NEWLINE
 
-dnf install rabbitmq-server -y &>>$LOG_FILE
-VALIDATE $? "INSTALLTION OF RABBITMQ"
+dnf install $app_name-server -y &>>$LOG_FILE
+VALIDATE $? "INSTALLTION OF $APP_NAME"
 NEWLINE
 
-systemctl enable rabbitmq-server &>>$LOG_FILE
-systemctl start rabbitmq-server &>>$LOG_FILE
-VALIDATE $? "ENABLING AND STARTING OF RABBITMQ"
+systemctl enable $app_name-server &>>$LOG_FILE
+systemctl start $app_name-server &>>$LOG_FILE
+VALIDATE $? "ENABLING AND STARTING OF $APP_NAME"
 NEWLINE
 
 echo -e "$Y PLEASE SETUP PASSWORD $N"
@@ -63,7 +28,4 @@ VALIDATE $? "USERNAME AND PASSWORD CREATION"
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
 VALIDATE $? "PERMISSIONS SET"
 
-END_TIME=$(date +%s)
-TOTAL_TIME=$(($END_TIME - $START_TIME))
-
-echo -e "$Y Total time taken to execute the script is $TOTAL_TIME seconds $N"
+PRINT_TIME
