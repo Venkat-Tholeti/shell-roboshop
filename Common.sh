698573459f1ccf62146fe7797e8fd36dfd4f30d3
venkat <devopsaws84s@gmail.com>
2025-12-12 19:16:59 +0530

    COMMON CODE

--- Common.sh
+++ Common.sh
@@ -14,7 +14,8 @@ echo -e "$G script started executing at $(date)" &>>$LOG_FILE
 echo -e "$Y Logs stored at  $LOG_FILE"
 
 USERID=$(id -u)
-check_root(){
+
+ROOTACCESS_CHECK(){
 if [ $USERID -ne 0 ]
 then 
    echo -e "$R ERROR: USER NEED TO SWITCH TO ROOT ACCESS $N" | tee -a $LOG_FILE
@@ -42,7 +43,63 @@ VALIDATE(){
     fi
 }
 
-print_time(){
+NODEJS_SETUP(){
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "DISABLE OLD MODULE OF NODEJS" 
+NEWLINE
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "ENABLE MODULE20 OF NODEJS"
+NEWLINE
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "INSTALLATION OF NODEJS"
+NEWLINE
+npm install &>>$LOG_FILE
+VALIDATE $? "UNZIP & DEPENDECNCIES  INSTALLATION"
+NEWLINE
+}
+
+APPUSERCODE_SETUP(){
+id roboshop
+if [ $? -ne 0 ]
+then 
+   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+   VALIDATE $? "CREATING APPLICATION USER"
+else
+   echo -e "$Y USER ALREADY EXIST, SKIPPING $N"
+fi
+
+NEWLINE
+mkdir -p /app &>>$LOG_FILE
+VALIDATE $? "APP DIRECTORY CREATION"
+NEWLINE
+
+rm -rf /app/* # removing any contents in the app directory if we rerun the script
+
+curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip  &>>$LOG_FILE
+VALIDATE $? "DOWNLOADING THE $APP_NAME APPLICATION CODE"
+NEWLINE
+
+cd /app &>>$LOG_FILE
+unzip /tmp/$app_name.zip &>>$LOG_FILE
+sleep 5
+echo -e "$Y UNZIPPING THE ZIP FILE $N"
+}
+
+SYSTEMD_SETUP(){
+cp $SCRIPT_DIRECTORY/$app_name.service /etc/systemd/system/$app_name.service &>>$LOG_FILE
+VALIDATE $? "COPYING $APP_NAME SERVICE"
+NEWLINE
+
+systemctl daemon-reload &>>$LOG_FILE
+sleep 5
+systemctl enable $app_name &>>$LOG_FILE
+systemctl start $app_name &>>$LOG_FILE
+VALIDATE $? "RELOAD,ENABLING & STARTING OF $APP_NAME SERVICE" 
+NEWLINE    
+}
+
+
+PRINT_TIME(){
 END_TIME=$(date +%s)
 TOTAL_TIME=$(($END_TIME - $START_TIME))
 NEWLINE
